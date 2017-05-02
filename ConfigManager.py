9a22bf1b322428a6a7a6d5cf2ecf755f051259e3
Manikandangithub-4991 <greatmani1994@gmail.com>
2017-05-02 18:56:25 +0530

    Update ConfigManager.py

--- ConfigManager.py
+++ ConfigManager.py
@@ -9,7 +9,7 @@ class ConfigManager(object):
     """
 
     url = "http://10.235.112.35:5003/api/v2/nlu/predict"
-    sop_predict_url = "http://10.235.112.36:8021/api/v1/sopsync/predict/"
+    sop_predict_url = "http://10.235.112.36:8021/api/v1/sopsync/predict"
     chatbot_url ="http://10.235.112.34:5003/HolmesIT/chatbot/v2/api"
     classifier_url = "http://10.235.112.36:8024/api/v1/Classifier/verifyAugBot"
     cogminer_url = "http://10.235.112.36:8090/api/v2/CogMiner/predict"
